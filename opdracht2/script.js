f22880c47184d9b8bc2acc183f1020049a3e8636
David van Oudheusden <d.vanoudheusden@rocva.nl>
2019-11-13 14:16:26 +0100

    oplossing opdracht 1 en opdracht 2

--- opdracht2/script.js
+++ opdracht2/script.js
@@ -5,7 +5,11 @@
  */
 
 /* jouw code hieronder-->> */
-var ingevuld_getal = ;
+
+
+var stem_leeftijd = 18;
+
+
 
 /* B) Schrijf code waarmee je het ingevulde getal vergelijkt (condition) met de stemgerechtigde leeftijd van 18 jaar.
  * Als de leeftijd groter is dan 18 dan verander je de achtergrondkleur van de site met:
@@ -13,7 +17,13 @@ var ingevuld_getal = ;
  */
 
 /* jouw code hieronder-->> */
+function check() {
+    var ingevuld_getal = document.getElementById('leeftijd').value;
 
+    if (ingevuld_getal > stem_leeftijd) {
+        document.getElementById("main").style.backgroundColor = "purple";
+    }
+}
 
 
 /* C) Schrijf code waarmee je het ingevulde getal vergelijkt (condition) met de stemgerechtigde leeftijd van 18 jaar.
--- uitleg/script.js
+++ uitleg/script.js
@@ -1,4 +1,24 @@
 
+var getal1 = 99;
+
+// document.getElementById("main").innerHTML = getal1;
+
+var getal2 = 78;
+
+getal1 = prompt("Wat is je leeftijd?");
+
+
+document.getElementById("toongetal").innerHTML = getal1;
+
+document.getElementById("invulveld1").value = getal1;
+
+var invulveld1 = document.getElementById("invulveld1").value;
+
+if (getal1 < invulveld1) { //TRUE OF FALSE
+    //dit is het blokje code
+}
+
+
 //Maak twee variabelen - volgorde van uitlezen door browser
 // Toon variabelen op het scherm
 // Vraag gebruiker om een INPUT waarde
